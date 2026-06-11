4e3014e7ff38467109cba3853fbfc61074d43771
Sellamuthu2007 <sellamuthur.cse2024@citchennai.net>
2026-06-11 08:15:47 +0530

    Fixed the errors thrown by CI pipeline

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
